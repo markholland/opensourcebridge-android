4ba08a0f841c6afedbfbf4330d551bf7d80176df
Mark Holland <mark@partiallogic.com>
2013-08-01 22:02:59 +1200

    Increased splash screen show length

--- src/org/osuosl/ocw/Main.java
+++ src/org/osuosl/ocw/Main.java
@@ -8,7 +8,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class Main extends Activity {
-        private final int SPLASH_DISPLAY_LENGTH = 100;
+        private final int SPLASH_DISPLAY_LENGTH = 500;
         /** Called when the activity is first created. */
         @Override
         public void onCreate(Bundle bundle) {

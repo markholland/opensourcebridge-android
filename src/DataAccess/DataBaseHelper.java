cedf61cd65062931bb7872454f8dc4dce9ab885f
Mark Holland <mark@partiallogic.com>
2014-02-10 16:41:24 +0100

    Removed generic methods from database helper, would impede using transactions

--- src/DataAccess/DataBaseHelper.java
+++ src/DataAccess/DataBaseHelper.java
@@ -9,7 +9,7 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 public class DataBaseHelper extends SQLiteAssetHelper implements IDataBaseHelper {
 
-	private static final String DATABASE_NAME = "ocw";     //Name of the .db file stored on the device
+	private static final String DATABASE_NAME = "ocw";  //Name of the .db file stored on the device
 	private static final int DATABASE_VERSION = 1;		   
 
 	private static DataBaseHelper mInstance;
@@ -30,110 +30,6 @@ public class DataBaseHelper extends SQLiteAssetHelper implements IDataBaseHelper
 		super(context.getApplicationContext(), DATABASE_NAME, null, DATABASE_VERSION);
 	}
 
-	/**
-	 * Inserts into a row of TABLE_NAME with the ContentValues values
-	 * @param TABLE_NAME
-	 * @param values
-	 * @return Error code from insertion
-	 */
-	public long insertRow(String TABLE_NAME, ContentValues values) {
-		SQLiteDatabase db = null;
-		Long i = 0l;
-
-		try {
-			db = this.getWritableDatabase();
-			db.beginTransaction();
-
-			i = db.insert(TABLE_NAME, null, values);
-			
-			db.setTransactionSuccessful();
-		} catch(Exception e) {
-			db.endTransaction();
-		}
-		db.endTransaction();
-
-		return i;
-	}
-
-
-	public long updateRow(String TABLE_NAME, int id,  ContentValues values) {
-		SQLiteDatabase db = null;
-		int i = 0;
-
-		try {
-			db = this.getWritableDatabase();
-			db.beginTransaction();
-
-			i = db.update(TABLE_NAME, values, "_id" + " = ?", new String[] { String.valueOf(id)});
-			
-			db.setTransactionSuccessful();
-		} catch(Exception e) {
-			db.endTransaction();
-		}
-		db.endTransaction();
-
-		return i;
-	}
-
-
-	public Cursor getCursor(String TABLE_NAME, String query) {
-
-		Cursor cursor = null;
-		SQLiteDatabase db = null;
-		try {
-			db = this.getReadableDatabase();
-			db.beginTransaction();
-			
-			cursor = db.rawQuery(query,null,null);
-			
-			db.setTransactionSuccessful();
-		}catch(Exception e){
-			db.endTransaction();
-		}
-		db.endTransaction();
-
-		return cursor;
-	}
 	
 	
-	public long deleteAllRowsFromTable(String TABLE_NAME) {
-		SQLiteDatabase db = null;
-		int i = 0;
-		try {
-			db = this.getReadableDatabase();
-			db.beginTransaction();
-
-			String deleteSQL = "DELETE FROM " +TABLE_NAME;
-			db.execSQL(deleteSQL);
-			
-			db.setTransactionSuccessful();
-
-			db.endTransaction();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		return i;
-	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
